72baa8e4e03e5828f4578c3301429169bc900dab
Timoyoungster <timo.proemer04@gmail.com>
2022-12-06 17:21:25 +0100

    changed cmp behaviour

--- plugin/cmp.lua
+++ plugin/cmp.lua
@@ -7,6 +7,11 @@ local check_backspace = function()
     return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
 end
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 cmp.setup({
     snippet = {
         expand = function(args)
@@ -15,6 +20,10 @@ cmp.setup({
     },
     window = {
         completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    view = {
+        entries = { name = 'custom', selection_order = 'near_cursor' }
     },
     mapping = cmp.mapping.preset.insert({
         ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
@@ -27,28 +36,27 @@ cmp.setup({
         }),
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
         ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            elseif check_backspace() then
-                fallback()
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
         ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            elseif check_backspace() then
-                fallback()
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-    }),
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+  },
     sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "luasnip" },
