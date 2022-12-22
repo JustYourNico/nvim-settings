64805901878e4eb2c4a00e9a9fde4a4e5107429b
Timoyoungster <timo.proemer04@gmail.com>
2022-12-22 23:15:48 +0100

    adding window mappings

--- lua/timoyoungster/remap.lua
+++ lua/timoyoungster/remap.lua
@@ -34,3 +34,29 @@ vim.keymap.set("v", "<leader>d", "\"_d")
 
 -- make executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- window movement
+vim.keymap.set("n", "<M-h>", "<C-w>h")
+vim.keymap.set("n", "<M-j>", "<C-w>j")
+vim.keymap.set("n", "<M-k>", "<C-w>k")
+vim.keymap.set("n", "<M-l>", "<C-w>l")
+
+vim.keymap.set("t", "<M-h>", "<cmd>wincmd h<CR>")
+vim.keymap.set("t", "<M-j>", "<cmd>wincmd j<CR>")
+vim.keymap.set("t", "<M-k>", "<cmd>wincmd k<CR>")
+vim.keymap.set("t", "<M-l>", "<cmd>wincmd l<CR>")
+
+-- resize window
+vim.keymap.set("n", "<M-Up>", ":resize -2<CR>")
+vim.keymap.set("n", "<M-Down>", ":resize +2<CR>")
+vim.keymap.set("n", "<M-Left>", ":vertical resize +2<CR>")
+vim.keymap.set("n", "<M-Right>", ":vertical resize -2<CR>")
+
+vim.keymap.set("t", "<M-Up>", "<cmd>resize -2<CR>")
+vim.keymap.set("t", "<M-Down>", "<cmd>resize +2<CR>")
+vim.keymap.set("t", "<M-Left>", "<cmd>vertical resize +2<CR>")
+vim.keymap.set("t", "<M-Right>", "<cmd>vertical resize -2<CR>")
+
+-- new window
+vim.keymap.set("n", "<M-v>", "<C-w>v")
+vim.keymap.set("n", "<M-s>", "<C-w>s")
