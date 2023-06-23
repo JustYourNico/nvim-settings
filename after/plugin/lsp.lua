ebf397e45c45906259166604ab38bb918379f0e9
Timoyoungster <timo.proemer04@gmail.com>
2023-06-23 21:09:23 +0200

    retab

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,9 +3,9 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.ensure_installed({
-	'quick_lint_js',
-	'pyright',
-	'rust_analyzer',
+  'quick_lint_js',
+  'pyright',
+  'rust_analyzer',
 })
 
 lsp.set_preferences({
@@ -13,18 +13,18 @@ lsp.set_preferences({
 })
 
 lsp.on_attach(function(client, bufnr)
-	local opts = {buffer = bufnr, remap = false}
+  local opts = {buffer = bufnr, remap = false}
 
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "<C-k>", function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set("n", "<leader>kws", function() vim.lsp.buf.workspace_symbol() end, opts)
-	vim.keymap.set("n", "<leader>kd", function() vim.diagnostic.open_float() end, opts)
-	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set("n", "<leader>kca", function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set("n", "<leader>krr", function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set("n", "<leader>krn", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "<C-k>", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>kws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>kd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>kca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>krr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>krn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
--- lua/timoyoungster/packer.lua
+++ lua/timoyoungster/packer.lua
@@ -4,70 +4,70 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
 
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-buffer'},
-			{'hrsh7th/cmp-path'},
-			{'saadparwaiz1/cmp_luasnip'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'hrsh7th/cmp-nvim-lua'},
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
 
-			-- Snippets
-			{'L3MON4D3/LuaSnip'},
-			{'rafamadriz/friendly-snippets'},
-		}
-	}
-
-	-- telescope your way through the filesystem
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.0',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-
-	-- prime colorscheme
-	use({
-		'rose-pine/neovim',
-		as = 'rose-pine',
-		config = function()
-			vim.cmd('colorscheme rose-pine')
-		end
-	})
-
-	-- sittin on a tree
-	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
-	use('nvim-treesitter/playground')
-	use('nvim-treesitter/nvim-treesitter-context')
-
-	-- fishin for files
-	use('theprimeagen/harpoon')
-
-	-- that big'ol tree
-	use('mbbill/undotree')
-
-    -- autopairs
-    use('windwp/nvim-autopairs')
-
-    -- todo highlighting
-    use {
-            "folke/todo-comments.nvim",
-            requires = "nvim-lua/plenary.nvim",
-            config = function()
-                    require("todo-comments").setup { }
-            end
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
     }
+  }
 
-    -- collaboration
-    use('jbyuki/instant.nvim')
+  -- telescope your way through the filesystem
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- prime colorscheme
+  use({
+    'rose-pine/neovim',
+    as = 'rose-pine',
+    config = function()
+      vim.cmd('colorscheme rose-pine')
+    end
+  })
+
+  -- sittin on a tree
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
+  use('nvim-treesitter/playground')
+  use('nvim-treesitter/nvim-treesitter-context')
+
+  -- fishin for files
+  use('theprimeagen/harpoon')
+
+  -- that big'ol tree
+  use('mbbill/undotree')
+
+  -- autopairs
+  use('windwp/nvim-autopairs')
+
+  -- todo highlighting
+  use {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+        require("todo-comments").setup { }
+      end
+  }
+
+  -- collaboration
+  use('jbyuki/instant.nvim')
 end)
